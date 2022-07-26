2d3d1a42bf43c334cea3911c6181bae21ddea033
Destiny Ryals <dcryals21@students.desu.edu>
2022-07-26 10:54:01 -0400

    Update

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,7 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +9,18 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+        int count = 0;
+        for(String word : input ){
+            if(!hashMap.containsKey(word)){
+                hashMap.put(word,0);
+            }else{
+                hashMap.put(word, hashMap.get(word)+1);
+                count++;
+            }
+        }
+
+        return count;
     }
 
     /**
@@ -16,6 +29,16 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        int count = 0;
+        for( int num : input){
+            if(!hashMap.containsKey(num)){
+                hashMap.put(num, 1);
+            }else{
+                hashMap.put(num, hashMap.get(num)+1);
+                count++;
+            }
+        }
+        return count;
     }
 }
